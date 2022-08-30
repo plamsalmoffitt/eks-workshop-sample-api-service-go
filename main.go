f764459195308e37a84dac868f56c3c53108a066
plamsalmoffitt <112419764+plamsalmoffitt@users.noreply.github.com>
2022-08-30 08:36:32 -0400

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World this is my first code pipeline deployment"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
